1b289cad8c73ae0d5bb2fb25b859257c2e25e1de
Leandro Gabriel Casas <leandrogabriel.casas@gmail.com>
2014-07-10 16:59:06 -0300

    Extended XML parser
    Extended XML to allow passing types that are not serializables, but have
    a serializable property. This allows to use the same classes for both
    the JSON parser and the XML parser.

--- SteamdotNet/Common/ISteamApps/Data/GetAppList.cs
+++ SteamdotNet/Common/ISteamApps/Data/GetAppList.cs
@@ -1,39 +1,39 @@
 ﻿namespace SteamdotNet.Common.ISteamApps.Data
 {
     /// <summary>
-    /// JSON: Root object
+    /// GetAppList: Root object
     /// </summary>
     public class GetAppList
     {
         /// <summary>
-        /// JSON: root.applist
+        /// GetAppList: root.applist
         /// </summary>
         public AppList Applist { get; set; }
     }
 
     /// <summary>
-    /// JSON: root.applist.apps
+    /// GetAppList: root.applist.apps
     /// </summary>
     public class App
     {
         /// <summary>
-        /// JSON: root.applist.apps[].appid
+        /// GetAppList: root.applist.apps[].appid
         /// </summary>
         public int Appid { get; set; }
         
         /// <summary>
-        /// JSON: root.applist.apps[].name
+        /// GetAppList: root.applist.apps[].name
         /// </summary>
         public string Name { get; set; }
     }
 
     /// <summary>
-    /// JSON: root.applist
+    /// GetAppList: root.applist
     /// </summary>
     public class AppList
     {
         /// <summary>
-        /// JSON: root.applist.apps
+        /// GetAppList: root.applist.apps
         /// </summary>
         public App[] Apps { get; set; }
     }
